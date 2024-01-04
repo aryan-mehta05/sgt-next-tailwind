59d1891bc886027cae4dea637fbed679fbc1ac29
Aryan Mehta <aryannmehta@gmail.com>
2024-01-04 17:49:26 +0530

    Add UserButton to dashboard-page

--- app/(auth)/dashboard/page.tsx
+++ app/(auth)/dashboard/page.tsx
@@ -1,7 +1,10 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
-    <main className='bg-black text-6xl'>
+    <main className='bg-black text-6xl text-white'>
       This is the Dashboard
+      <UserButton afterSignOutUrl="/sign-in" />
     </main>
   );
 };
